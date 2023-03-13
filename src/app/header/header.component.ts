7ae22fbace5436db8a8b1a7add1186276dcdf278
jeevanandam <jeeva2434@gmail.com>
2023-03-13 11:32:34 +0530

    header component design started

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  // menuToggle = document.getElementById('menu-toggler');
+  menuToggler(event: any):void{
+    console.log('clickk');
+    this.toggleBodyClass("menu-active");
+  }
+
+  toggleBodyClass(className: string) {
+    let activeElement:any = document.getElementById('head_wrapper');
+    activeElement.classList.toggle(className);
   }
 
 }
