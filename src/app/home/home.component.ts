0597fc541bc0b4b9b39837af3cc512a606021c0e
jeeva <jeevanandam.s@heptagon.in>
2023-03-16 01:39:27 +0530

    home about page design

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,4 +13,74 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
    
   }
+
+  menuItemList = [
+    {
+      name: 'Breakfast',
+      router: 'breakfast'
+    },
+    {
+      name: 'Lunch',
+      router: 'lunch'
+    },
+    {
+      name: 'Dinner',
+      router: 'dinner'
+    },
+    {
+      name: 'kerala lunch',
+      router: 'kerala_lunch'
+    },
+    {
+      name: 'Payasam Varieties',
+      router: 'payasam _varieties'
+    },
+    {
+      name: 'Sweets',
+      router: 'sweets'
+    },
+    {
+      name: 'Dosa Varieties',
+      router: 'dosa_varieties'
+    },
+    {
+      name: 'Gravy Varieties',
+      router: 'gravy_varieties'
+    },
+    {
+      name: 'Raitha Varieties',
+      router: 'raitha_varieties'
+    },
+    {
+      name: 'Rice Receipe Varieties',
+      router: 'rice_receipe_varieties'
+    }
+  ];
+
+  whatWeDo = [
+    {
+      name:'Engagement',
+      route:'engagement'
+    },
+    {
+      name:'Wedding',
+      route:'wedding'
+    },
+    {
+      name:'Baby Shower',
+      route:'baby_shower'
+    },
+    {
+      name:'House Warming',
+      route:'house_warming'
+    },
+    {
+      name:'Birthday Party',
+      route:'birthday_party'
+    },
+    {
+      name:'Corporate Events',
+      route:'corporate_events'
+    }
+  ]
 }
